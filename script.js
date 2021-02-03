c958e5285d6cdeae16e337426577511e0405422d
BeckyLashua <beckylashua@gmail.com>
2021-02-03 15:16:30 -0500

    added a tbody to my table

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// Create border and highlight top left corner (maybe use function for highlighting a cell)
-// highlightCell()
-
+// Function for creating a table and appending it to body
 function addTable() {
   // craete table element
   let newTable = document.createElement("table");
+  let newTableBody = document.createElement("tbody");
   newTable.style.border = "1px solid black";
 
   // create 4 rows 
   for (let i = 0; i < 4; i++) {
     let row = document.createElement("tr");
-    newTable.appendChild(row);
+    newTableBody.appendChild(row);
   }
   // create headers and append them 
   for (let i = 1; i <= 4; i++) {
     let header = document.createElement("th");
     header.textContent = "Header " + i;
-    newTable.children[0].appendChild(header);
+    newTableBody.children[0].appendChild(header);
     header.style.border = "1px solid black";
   }
   // create cells 
@@ -23,14 +22,16 @@ function addTable() {
     for (let j = 1; j <= 4; j++) {
       let cell = document.createElement("td");
       cell.textContent = i + "," + j;
-      newTable.children[i].appendChild(cell);
+      newTableBody.children[i].appendChild(cell);
       cell.style.border = "1px solid black";
     }
   }
   
   // append to body 
+  newTable.appendChild(newTableBody);
   document.body.appendChild(newTable);
   newTable.setAttribute("id", "newTable");
+  newTableBody.setAttribute("id", "newTableBody");
 }
 addTable();
 //document.body.appendChild(newTable);
@@ -53,35 +54,39 @@ let rightButton = document.createElement("button");
 rightButton.textContent = "Right";
 buttonDiv.appendChild(rightButton);
 
+// change border function for directional buttons
+function changeColor(selected, newSelected) {
+  if (newSelected != null) {
+    newSelected.style.border = "5px solid black";
+    selected.style.border = "1px solid black";
+    newSelected.setAttribute("id", "selected");
+    selected.removeAttribute("id", "selected");
+  }
+}
+
 // Add event listener for each button
 upButton.addEventListener("click", () => {
-
+  let selected = document.getElementById("selected");
+  let newSelected = selected.parentNode;
+  changeColor(selected, newSelected); 
 });
 
 downButton.addEventListener("click", () => {
-
+  let selected = document.getElementById("selected");
+  let newSelected = selected.parentNode;
+  changeColor(selected, newSelected);
 });
 
 leftButton.addEventListener("click", () => {
   let selected = document.getElementById("selected");
   let newSelected = selected.previousSibling;
-  if (newSelected != null) {
-    newSelected.style.border = "5px solid black";
-    selected.style.border = "1px solid black";
-    newSelected.setAttribute("id", "selected");
-    selected.removeAttribute("id", "selected");
-  }
+  changeColor(selected, newSelected);
 });
 
 rightButton.addEventListener("click", () => {
   let selected = document.getElementById("selected");
   let newSelected = selected.nextSibling;
-  if (newSelected != null) {
-    newSelected.style.border = "5px solid black";
-    selected.style.border = "1px solid black";
-    newSelected.setAttribute("id", "selected");
-    selected.removeAttribute("id", "selected");
-  }
+  changeColor(selected, newSelected);
 });
 
 // Create button labeled "Marked Cell"
@@ -96,17 +101,7 @@ markCellButton.addEventListener("click", () => {
 
 // When the page is loaded, the upper-left non-header cell of the table should be 'selected'.
 // This is denoted by it having a thicker border than the other cells
-let highlightedCell = document.getElementById("newTable").children[1].children[0];
+let highlightedCell = document.getElementById("newTableBody").children[1].children[0];
 highlightedCell.style.border = "5px solid black";
 highlightedCell.setAttribute("id", "selected");
-// If you push the directional buttons, other cells should be selected instead. 
-//So if you press the right button, cell 1,1 should no longer be selected 
-// and 1, 2 should be selected instead. (siblings??)
-
-// If you are already on the top row and hit 'up' nothing should happen 
-//(you should not be able to move into the header cells). 
-//Likewise if you are all the way right and hit right or all the way at the bottom and hit down.
-
-// Hitting the "Mark Cell" button should permanently change the background 
-// of the selected cell to yellow. This should persist even after other cells are selected or marked.
 
